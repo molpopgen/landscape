0a527d879419195b4d36c4839279648ec2ec699e
molpopgen <krthornt@uci.edu>
2016-07-22 15:59:06 -0700

    change value.  add comments

--- src/simtypes.hpp
+++ src/simtypes.hpp
@@ -7,7 +7,7 @@
 namespace bg = boost::geometry;
 
 using point = bg::model::point<double, 2, bg::cs::cartesian>;
-using value = std::pair<point, unsigned>;
+using value = std::pair<point, std::size_t>;
 
 struct diploid : public KTfwd::tags::custom_diploid_t
 /*
@@ -15,9 +15,9 @@ struct diploid : public KTfwd::tags::custom_diploid_t
  * that it gets "dispatched" properly.
  */
 {
-    std::size_t first;
-    std::size_t second;
-    value v;
+    std::size_t first; //first gamete
+    std::size_t second;//second gamete
+    value v;           //location in space & index in population
 };
 
 #endif
