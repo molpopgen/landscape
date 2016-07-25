8237c50c211d8daa3608a325315de4823e973174
Kevin R. Thornton <krthornt@uci.edu>
2016-07-25 11:12:33 -0700

    change name of diploid type.  Define the type of our population.

--- src/simtypes.hpp
+++ src/simtypes.hpp
@@ -6,15 +6,17 @@
 #include <boost/geometry/geometries/point.hpp>
 #include <boost/geometry/core/cs.hpp>
 #include <fwdpp/tags/diploid_tags.hpp>
+#include <fwdpp/sugar/singlepop.hpp>
+#include <fwdpp/sugar/popgenmut.hpp>
 
 namespace bg = boost::geometry;
 
 using point = bg::model::point<double, 2, bg::cs::cartesian>;
 using value = std::pair<point, std::size_t>;
 
-struct diploid : public KTfwd::tags::custom_diploid_t
+struct csdiploid : public KTfwd::tags::custom_diploid_t
 /*
- * Minimal custom diploid. Inherits fwdpp tag so
+ * Minimal custom diploid in Cartesian space. Inherits fwdpp tag so
  * that it gets "dispatched" properly.
  */
 {
@@ -23,11 +25,12 @@ struct diploid : public KTfwd::tags::custom_diploid_t
     first_type first; //first gamete
     second_type second;//second gamete
     value v;           //location in space & index in population
-    diploid() noexcept : v(std::make_pair(point(std::numeric_limits<double>::quiet_NaN(),
+    csdiploid() noexcept : v(std::make_pair(point(std::numeric_limits<double>::quiet_NaN(),
                                               std::numeric_limits<double>::quiet_NaN()),
                                               std::numeric_limits<std::size_t>::max()))
     {
     }
 };
 
+using poptype = KTfwd::singlepop<KTfwd::popgenmut,csdiploid>;
 #endif
