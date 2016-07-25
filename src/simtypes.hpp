57d955d609c829bfb910cd5c6be6f50922cca9cc
Kevin R. Thornton <krthornt@uci.edu>
2016-07-25 11:15:51 -0700

    add namespace

--- src/simtypes.hpp
+++ src/simtypes.hpp
@@ -11,6 +11,8 @@
 
 namespace bg = boost::geometry;
 
+namespace landscape
+{
 using point = bg::model::point<double, 2, bg::cs::cartesian>;
 using value = std::pair<point, std::size_t>;
 
@@ -26,16 +28,16 @@ struct csdiploid : public KTfwd::tags::custom_diploid_t
     second_type second;//second gamete
     value v;           //location in space & index in population
     csdiploid() noexcept : v(std::make_pair(point(std::numeric_limits<double>::quiet_NaN(),
-                                              std::numeric_limits<double>::quiet_NaN()),
-                                              std::numeric_limits<std::size_t>::max()))
+                                                std::numeric_limits<double>::quiet_NaN()),
+                                                std::numeric_limits<std::size_t>::max()))
     {
     }
 };
 
 /*
  * Our population is a single deme.  The mutation type
- * is KTfwd::popgenmut, from fwdpp/sugar/popgenmut.hpp.  
- * This mutation is the "standard" mutation type with 
+ * is KTfwd::popgenmut, from fwdpp/sugar/popgenmut.hpp.
+ * This mutation is the "standard" mutation type with
  * position, s, h as its main data members.  The origin
  * time of mutations is also recorded.
  *
@@ -47,4 +49,5 @@ struct csdiploid : public KTfwd::tags::custom_diploid_t
  * The fwdpp manual online has detailed info, too.
  */
 using poptype = KTfwd::singlepop<KTfwd::popgenmut,csdiploid>;
+}
 #endif
