d1423f471607ba434194e5c1aa6b89f5ef6bd5fb
Kevin R. Thornton <krthornt@uci.edu>
2016-07-25 12:03:18 -0700

    make typedefs members of csdiploid

--- src/simtypes.hpp
+++ src/simtypes.hpp
@@ -9,20 +9,19 @@
 #include <fwdpp/sugar/singlepop.hpp>
 #include <fwdpp/sugar/popgenmut.hpp>
 
-namespace bg = boost::geometry;
+
 
 namespace landscape
 {
-using point = bg::model::point<double, 2, bg::cs::cartesian>;
-using value = std::pair<point, std::size_t>;
-
 struct csdiploid : public KTfwd::tags::custom_diploid_t
 /*
  * Minimal custom diploid in Cartesian space. Inherits fwdpp tag so
  * that it gets "dispatched" properly.
  */
 {
-    using first_type = std::size_t;
+	using point = boost::geometry::model::point<double, 2, boost::geometry::cs::cartesian>;
+	using value = std::pair<point, std::size_t>;
+	using first_type = std::size_t;
     using second_type = std::size_t;
     first_type first; //first gamete
     second_type second;//second gamete
